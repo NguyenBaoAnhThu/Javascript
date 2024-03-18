8654bb20c31badd191a6587eccf4aadbc821f9b6
NguyenBaoAnhThu <125013548+NguyenBaoAnhThu@users.noreply.github.com>
2024-03-18 16:37:57 +0700

    Add files via upload

---
+++ JAVASCRIP/test.js
@@ -0,0 +1,12 @@
+// let a = +prompt("Nhap gia tri a")
+// let b = +prompt("Nhap gia tri b")
+// if ( a==0 ){
+//     if ( b == 0 ){
+//         alert("Vo so nghiem")
+//     } else {
+//         alert("Vo nghiem")
+//     }
+// }else {
+//     let x=-b/a;
+//     alert(x)
+// }
